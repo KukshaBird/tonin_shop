e1b0e9878930ff542ec4f1138feb1a1a1407bf0b
kukshabird <samoilovartem1989@gmail.com>
2019-08-07 13:41:03 +0300

    A-Z

--- items/filters.py
+++ items/filters.py
@@ -6,8 +6,8 @@ from .models import Product
 class ProductFilter(django_filters.FilterSet):
 
 	CHOICES = (
-			('ascending', 'От новых к старым'),
-			('descending', 'От старых к новым')
+			('ascending', 'А - Я'),
+			('descending', 'Я - А')
 		)
 
 	ordering = django_filters.ChoiceFilter(label='Сортировать:', choices=CHOICES, method='filter_by_order')
@@ -21,7 +21,7 @@ class ProductFilter(django_filters.FilterSet):
 		}
 
 	def filter_by_order(self, queryset, name, value):
-		expression = 'create_date' if value == 'ascending' else '-create_date'
+		expression = 'name' if value == 'ascending' else '-name'
 		return queryset.order_by(expression)
 
 
