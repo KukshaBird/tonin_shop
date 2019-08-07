f3a6c41b10aa62ebe96d189dd033e569364fdc1c
kukshabird <samoilovartem1989@gmail.com>
2019-08-07 20:32:54 +0300

    go to test filters

--- items/filters.py
+++ items/filters.py
@@ -1,5 +1,5 @@
 import django_filters
-from .models import Product
+from .models import Product,Towel
 
 
 
@@ -11,9 +11,33 @@ class ProductFilter(django_filters.FilterSet):
 		)
 
 	ordering = django_filters.ChoiceFilter(label='Сортировать:', choices=CHOICES, method='filter_by_order')
-
+	price = django_filters.RangeFilter(label='Цена между')
 	class Meta:
 		model = Product
+		fields = {
+			'name': ['icontains'],
+			'article':['icontains'],
+			# 'price': ['range'],
+			'fabric': ['exact'],
+			'type': ['exact'],
+			'available':['exact']
+		}
+
+	def filter_by_order(self, queryset, name, value):
+		expression = 'name' if value == 'ascending' else '-name'
+		return queryset.order_by(expression)
+
+class TowelFilter(django_filters.FilterSet):
+
+	CHOICES = (
+			('ascending', 'А - Я'),
+			('descending', 'Я - А')
+		)
+
+	ordering = django_filters.ChoiceFilter(label='Сортировать:', choices=CHOICES, method='filter_by_order')
+
+	class Meta:
+		model = Towel
 		fields = {
 			'name': ['icontains'],
 			'price': ['lt','gt'],
@@ -23,5 +47,6 @@ class ProductFilter(django_filters.FilterSet):
 	def filter_by_order(self, queryset, name, value):
 		expression = 'name' if value == 'ascending' else '-name'
 		return queryset.order_by(expression)
+
 
 
--- items/views.py
+++ items/views.py
@@ -2,8 +2,9 @@ from django.views.generic import ListView,DetailView
 #from django.http import HttpResponse
 from items.models import Product,Towel
 #from tonin_shop.views import Index
-from .filters import ProductFilter
+from .filters import ProductFilter,TowelFilter
 from django.core.paginator import Paginator
+from .forms import ProductFilterForm
 
 
 class ProductListView(ListView):
@@ -12,9 +13,11 @@ class ProductListView(ListView):
     context_object_name = 'product_list'
     paginate_by = 6
 
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['filter'] = ProductFilter(self.request.GET, queryset=self.get_queryset())
+        context['form'] = ProductFilterForm
         Paginator(context['filter'], self.paginate_by)
 
         return context
@@ -24,6 +27,11 @@ class TowelListView(ListView):
     template_name = 'towels_list.html'
     paginate_by = 6
     queryset = Towel.objects.all().order_by('-create_date')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = TowelFilter(self.request.GET, queryset=self.get_queryset())
+        return context
 
 class ProductDetailView(DetailView):
     model = Product
